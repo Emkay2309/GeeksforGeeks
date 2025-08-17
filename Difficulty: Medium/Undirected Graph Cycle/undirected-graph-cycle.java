d497bc84707c1831805b0c2d50b2582483c6d19e
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-08-18 04:21:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,81 +1,56 @@
 class Solution {
     public boolean isCycle(int V, int[][] edges) {
         // Code here
-        List<List<Integer>> adj = createAdj(V , edges);
-        boolean [] vis = new boolean [V];
-        
-        for(int i=0 ; i<V ; i++) {
-            if(!vis[i]) {
-                //boolean check = dfs(i , -1, vis , adj);
-                boolean check1 = bfs( i , vis , adj);
-                if(check1) return true;
-                //if(check) return true;
-            }
-        }
-        return false;
-    }
-    
-    public boolean bfs(int curr , boolean [] vis , List<List<Integer>> adj) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(curr , -1));
-        vis[curr] = true;
-        
-        while(!q.isEmpty()) {
-            Pair currPair = q.poll();
-            
-            int node = currPair.node;
-            int par = currPair.par;
-            
-            for(int neigh : adj.get(node)) {
-                if(!vis[neigh]) {
-                    q.add(new Pair(neigh , node));
-                    vis[neigh] = true;
-                }
-                else if(neigh != par) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    
-    public boolean dfs(int curr , int par , boolean [] vis , List<List<Integer>> adj ) {
-        vis[curr] = true;
-        
-        for(int neigh : adj.get(curr)) {
-            if(!vis[neigh]) {
-                if(dfs(neigh , curr , vis , adj) )   return true;
-            }
-            else if (par != neigh) {
+         DSU dsu = new DSU(V);
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (!dsu.union(u, v)) {
                 return true;
             }
         }
         return false;
     }
-    
-    public List<List<Integer>> createAdj(int n , int[][]edge) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0 ; i<n ; i++) {
-            adj.add(new ArrayList<>());
-        }
-        
-        for(int e [] : edge) {
-            int sv = e[0];
-            int ev = e[1];
-            
-            adj.get(sv).add(ev);
-            adj.get(ev).add(sv);
-        }
-        return adj;
-    }
 }
 
-class Pair {
-    int node;
-    int par;
-    
-    Pair(int node , int par) {
-        this.node = node;
-        this.par = par;
+class DSU {
+    int[] parent;
+    int[] rank;
+
+    DSU(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // path compression
+        }
+        return parent[x];
+    }
+
+    public boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) return false; // already connected → cycle
+
+        // union by rank
+        if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+
+        return true;
     }
 }
