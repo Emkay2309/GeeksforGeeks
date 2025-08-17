63a158eadd6f6c8c23d5ed97052d9672dd19b306
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-08-18 00:18:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -2,33 +2,38 @@ class Solution {
     public boolean isCyclic(int V, int[][] edges) {
         // code here
         List<List<Integer>> adj = createAdj(V , edges);
-        boolean [] vis = new boolean [V];
-        boolean [] rec = new boolean [V];    
         
+        int [] indegree = new int [V];
+        for(int [] e : edges) {
+            indegree[e[1]]++;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
         for(int i=0 ; i<V ; i++) {
-            if(!vis[i]) {
-                boolean check = dfs(i , rec, vis , adj);
-                if(check) return true;
+            if(indegree[i] == 0) {
+                q.add(i);
             }
         }
-        return false;
+        
+        int count = 0;
+        
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            count++;
+            
+            for(int neigh : adj.get(curr)) {
+                indegree[neigh]--;
+                if(indegree[neigh] == 0){
+                    q.add(neigh);
+                }
+            }
+        }
+        
+        if(count == V) return false;
+        return true;
     }
     
-    public boolean dfs(int curr , boolean [] rec , boolean [] vis , List<List<Integer>> adj) {
-        vis[curr] = true;
-        rec[curr] = true;
-        
-        for(int neigh : adj.get(curr)) {
-            if(!vis[neigh]) {
-                if(dfs(neigh , rec , vis ,adj)) return true;
-            }
-            else if(rec[neigh]) {
-                return true;
-            } 
-        }
-        rec[curr] = false;
-        return false;
-    }
+    
     
     
     
