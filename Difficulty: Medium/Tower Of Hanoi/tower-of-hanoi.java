d915941075c6a0bb476243c8f628130367fa95d7
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-08-24 18:14:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.java
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.java
@@ -1,7 +1,7 @@
 class Solution {
     public int towerOfHanoi(int n, int from, int to, int aux) {
         // code here
-        if(n == 0) return 0;
-        return 2*towerOfHanoi(n-1,from,to ,aux) + 1;
+        if(n == 0 || n == 1) return n;
+        return (int) Math.pow(2,n)-1;
     }
 }
