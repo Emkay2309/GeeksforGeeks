2ba27dbfae23d29edcc28bee932528bb6359f908
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-08-24 22:26:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -1,85 +1,50 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            ArrayList<Integer> array1 = new ArrayList<Integer>();
-            String line = read.readLine();
-            String[] tokens = line.split(" ");
-            for (String token : tokens) {
-                array1.add(Integer.parseInt(token));
-            }
-            ArrayList<Integer> v = new ArrayList<Integer>();
-            int[] arr = new int[array1.size()];
-            int idx = 0;
-            for (int i : array1) arr[idx++] = i;
-
-            new Solution().mergeSort(arr, 0, arr.length - 1);
-
-            for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
-
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
 
     void mergeSort(int arr[], int l, int r) {
+        // code here
+        if(l == r) return;
         
-        if(l >= r)    return;
         int mid = (l+r)/2;
         
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid+1, r);
+        mergeSort(arr , l , mid);
+        mergeSort(arr , mid+1 , r);
         
-        merge(arr, l, mid, r);
+        merge(arr , l , mid , r);
     }
     
-    void merge(int[] arr, int low, int mid, int high)  {
+    void merge(int [] arr , int l , int mid , int r) {
+        int n1 = mid-l+1;
+        int n2 = r-mid;
         
-        int left = low;
-        int right = mid+1;
+        int [] left = new int [n1];
+        int [] right = new int [n2];
         
-        ArrayList<Integer> temp = new ArrayList<>();
-    
-        while (left <= mid  &&  right <= high )  {
-            if(arr[left] < arr[right])  {
-                temp.add(arr[left]);
-                left++;
+        for(int i=0 ; i<n1 ; i++) {
+            left[i] = arr[l+i];
+        }
+        
+        for(int i=0; i<n2 ; i++) {
+            right[i] = arr[mid+1+i];
+        }
+        
+        int k = l;
+        int i = 0 , j = 0;
+        
+        while(i<n1 && j<n2) {
+            if(left[i] <= right[j]) {
+                arr[k++] = left[i++];
             }
             else {
-                temp.add(arr[right]);
-                right++;
+                arr[k++] = right[j++];
             }
         }
         
-        while(left <= mid){
-            temp.add(arr[left]);
-            left++;
+        while( i < n1 ) {
+            arr[k++] = left[i++];
         }
         
-        while(right <= high){
-            temp.add(arr[right]);
-            right++;
-        }
-        
-        for(int i=low; i<=high; i++) {
-            arr[i] = temp.get(i-low);
+        while( j < n2)  {
+            arr[k++] = right[j++];
         }
     }
 }
