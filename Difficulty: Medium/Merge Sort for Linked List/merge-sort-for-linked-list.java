a6ca27922d13f80532858f3f343cb71f6286bbbd
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-09-08 15:23:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
@@ -38,22 +38,16 @@ class Solution {
 
     // Merge two sorted linked lists
     private Node merge(Node l1, Node l2) {
-        Node dummy = new Node(0);
-        Node tail = dummy;
-
-        while (l1 != null && l2 != null) {
-            if (l1.data <= l2.data) {
-                tail.next = l1;
-                l1 = l1.next;
-            } else {
-                tail.next = l2;
-                l2 = l2.next;
-            }
-            tail = tail.next;
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        
+        if(l1.data <= l2.data) {
+            l1.next = merge(l1.next , l2);
+            return l1;
         }
-        if (l1 != null) tail.next = l1;
-        if (l2 != null) tail.next = l2;
-
-        return dummy.next;
+        else {
+            l2.next = merge(l1 , l2.next);
+            return l2;
+        }
     }
 }
