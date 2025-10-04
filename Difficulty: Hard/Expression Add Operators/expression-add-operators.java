3fc3c52ae5143fed537a09f66054f5e41ea3847b
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-10-04 11:45:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Expression Add Operators/expression-add-operators.java
@@ -0,0 +1,37 @@
+class Solution {
+    static ArrayList<String> aa;
+    
+    public static ArrayList<String> findExpr(String S, int target) {
+        
+        aa = new ArrayList<>();
+        addOperatorsR(0,S,target,"",0,0);
+        return aa;
+    }
+    
+    public static void addOperatorsR(int idx, String S, int target,String path,long val,long prev){
+        
+        if(idx==S.length()){
+            if(val==target){
+                aa.add(path);
+            }
+            return;
+        }
+        
+        for(int i=idx;i<S.length();i++){
+            
+            String c= S.substring(idx,i+1);
+            long cV= Long.parseLong(c);
+            
+            if(idx==0){
+                addOperatorsR(i+1,S,target,path+c,val+cV,cV);
+            }else{
+                addOperatorsR(i+1,S,target,path+"+"+c,val+cV,cV);
+                addOperatorsR(i+1,S,target,path+"-"+c,val-cV,-cV);
+                addOperatorsR(i+1,S,target,path+"*"+c,val-prev+prev*cV,prev*cV);
+            }
+            
+            if(c.charAt(0)=='0') break;
+            
+        }
+    }
+}
