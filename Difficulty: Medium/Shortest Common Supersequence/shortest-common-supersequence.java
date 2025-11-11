2ef0546a26359d0cc5200710503fe9a810268428
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-11-11 10:55:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.java
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.java
@@ -1,37 +1,23 @@
-// User function Template for Java
-
 class Solution {
-    // Function to find length of shortest common supersequence of two strings.
-    public static int shortestCommonSupersequence(String s1, String s2) {
-        // Your code here
+    public static int minSuperSeq(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
         
-        int [] [] dp = new int [n+1][m+1];
-        for(int [] a : dp) {
-            Arrays.fill(a , -1);
+        int [][]dp = new int [n + 1][m + 1];
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
         }
+        //dp[n][m] + (n - dp[n][m]) + (m - dp[n][m]); you can simplify the equation
+        int result = n + m - dp[n][m];//finally simplified
         
-        int val = dfs(n, m, s1, s2, dp);
-        
-        return n+m-val;
-    }
-    
-    static int dfs(int n , int m , String s1 , String s2 , int [] [] dp) {
-        if(n < 0 || m < 0) return -1;
-        
-        if(n==0 || m==0) return 0;
-        
-        if(dp[n][m] != -1) return dp[n][m];
-        
-        if(s1.charAt(n-1) == s2.charAt(m-1)) {
-            return dp[n][m] =  1 + dfs(n-1 , m-1 , s1 , s2 , dp);    
-        }
-        else {
-            int val1 = dfs(n-1 , m , s1 ,s2 , dp);
-            int val2 = dfs(n , m-1 , s1 ,s2 , dp);
-            
-            return dp[n][m] =  Math.max(val1,val2);
-        }
+        return result;
     }
 }
