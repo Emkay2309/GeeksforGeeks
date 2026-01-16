300235306fbd4b2d3e8a23d42b6e47de4a041831
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2026-01-16 10:06:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Candy/candy.java
+++ Difficulty: Hard/Candy/candy.java
@@ -9,27 +9,21 @@ class Solution {
         Arrays.fill(left,1);
         Arrays.fill(right,1);
         
-        for(int i = 1; i< arr.length; i++)
-        {
-            if(arr[i] > arr[i-1])
-            {
+        for(int i = 1; i< arr.length; i++){
+            if(arr[i] > arr[i-1]){
                 left[i] += left[i-1];
             }
-            
         }
         
-        for(int i = arr.length - 2; i>=0; i--)
-        {
-            if(arr[i] > arr[i+1])
-            {
+        for(int i = arr.length - 2; i>=0; i--){
+            if(arr[i] > arr[i+1]){
                 right[i] += right[i+1];
             }
         }
         
         int maxCandies = 0;
         
-        for(int i = 0; i<left.length; i++)
-        {
+        for(int i = 0; i<left.length; i++){
             maxCandies += Math.max(left[i], right[i]);
         }
         return maxCandies;
