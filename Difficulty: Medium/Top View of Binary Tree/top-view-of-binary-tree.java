183e511f9eecc0ac9bc3c0777c24f926badd7ebb
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2026-03-15 19:00:42 -0400

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -8,63 +8,42 @@ class Node {
         this.left = null;
         this.right = null;
     }
-    
-    Hashmap<Integer,INteger> 
-    HashMap<Node , INteger> 
-    
-    map -->  0 -  10
-            -1- 20
-            -2- 40
-            1 - 30
-            2 - 100
 }
 */
 
 class Solution {
+    static class Pair {
+        int data, depth;
+        Pair(int d, int dep) {
+            data = d;
+            depth = dep;
+        }
+    }
+
+    Map<Integer, Pair> map;
     ArrayList<Integer> ans;
+
     public ArrayList<Integer> topView(Node root) {
         ans = new ArrayList<>();
-        if(root == null) return ans;
-        
-        int min = 0 , max = 0;
-        
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root , 0)); // (10,0)
-        Map<Integer,Integer> map = new HashMap<>();
-        
-        while(!q.isEmpty()) {
-            Pair curr = q.poll(); //   (10,0) .; (20,-2)
-            Node currNode = curr.node; //10 , 20
-            int hd = curr.hd; //0  , -2
-            
-            min = Math.min(hd , min);
-            max = Math.max(hd , max);
-            
-            if(map.containsKey(hd) == false) {
-                map.put(hd , currNode.data);  // 0 -> 10
-            }
-            
-            
-            if(currNode.left != null) {
-                q.add(new Pair(currNode.left , hd-1)); // (20,-1)
-            }
-            if(currNode.right != null) {
-                q.add(new Pair(currNode.right , hd+1)); // (30,1)
-            }
-        }
-        
-        for(int i=min ; i<=max ; i++) {
-            ans.add(map.get(i));
+        map = new TreeMap<>();
+
+        dfs(root, 0, 0);
+
+        for (Pair p : map.values()) {
+            ans.add(p.data);
         }
         return ans;
     }
-}
 
-class Pair {
-    Node node;
-    int hd;
-    Pair(Node node , int hd) {
-        this.node = node;
-        this.hd = hd;
+    // hd = horizontal distance, depth = level from root
+    public void dfs(Node root, int hd, int depth) {
+        if (root == null) return;
+
+        if (!map.containsKey(hd) || map.get(hd).depth > depth) {
+            map.put(hd, new Pair(root.data, depth));
+        }
+
+        dfs(root.left, hd - 1, depth + 1);
+        dfs(root.right, hd + 1, depth + 1);
     }
 }
