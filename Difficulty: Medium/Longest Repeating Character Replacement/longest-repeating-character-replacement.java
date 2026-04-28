918708675c70fd7af0f598b5bf9ed94a9583c9b8
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2026-04-28 06:32:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Repeating Character Replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int longestSubstr(String s, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        int maxLen = 0, maxFreq = 0, left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            maxFreq = Math.max(maxFreq, map.get(ch));
+
+            while ((right - left + 1) - maxFreq > k) {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
