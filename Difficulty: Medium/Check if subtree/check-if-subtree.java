529ffa70f2c8e1efbf9ec53c6ae34064c7431553
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2026-05-07 13:18:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check if subtree/check-if-subtree.java
@@ -0,0 +1,31 @@
+/*
+Definition for Node
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int x) {
+        data = x;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    private boolean isSame(Node a, Node b) {
+        if (a == null && b == null) return true;
+        if (a == null || b == null) return false;
+        return a.data == b.data && 
+               isSame(a.left, b.left) && 
+               isSame(a.right, b.right);
+    }
+    
+    public boolean isSubTree(Node root1, Node root2) {
+        if (root2 == null) return true;
+        if (root1 == null) return false;
+        
+        if (isSame(root1, root2)) return true;
+        
+        return isSubTree(root1.left, root2) || isSubTree(root1.right, root2);
+    }
+}
